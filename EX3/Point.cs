4df3d7a3a4a2476982c737c937234cc0a7fd3690
amina charki <amina charki@AMINA>
2022-12-03 20:48:39 +0100

    te

---
+++ EX3/Point.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX3
+{
+   public  class Point
+    {
+        private int abscisse;
+        private int ordonnée;
+
+        public Point (int x , int y)
+        {
+            abscisse = x;
+            ordonnée = y;
+        }
+        //accesseurs 
+        public int absciss
+        {
+            set { abscisse = value; }
+            get { return abscisse; }
+        }
+        public int ordonn
+        {
+            set { ordonnée = value; }
+            get { return ordonnée; }
+        }
+        
+        public void translit(int dx ,int dy)
+        {
+            abscisse += dx;
+            ordonnée += dy;
+
+        }
+        public double distanceorg()
+        {
+            return Math.Sqrt(abscisse * abscisse + ordonnée * ordonnée);
+        }
+        public double disTtanceautrepoint(Point B)
+        {
+            return Math.Sqrt((abscisse - B.abscisse) * (abscisse - B.abscisse) + (ordonnée - B.ordonnée) * (ordonnée - B.ordonnée));
+
+        }
+
+
+    }
+}
--- EX3/Program.cs
+++ EX3/Program.cs
@@ -1,15 +1,54 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
 using System.Threading.Tasks;
+using System.Security.Principal;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.NetworkInformation;
 
-namespace EX3
+namespace cercle
 {
-    internal class Program
+    public class Point
+{
+    private int X;
+    private int Y;
+    private int originalX;
+    private int originalY;
+    public void Reset()
     {
-        static void Main(string[] args)
-        {
-        }
+        X = originalX;
+        Y = originalY;
+    }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+        originalX = X;
+        originalY = Y;
+    }
+    public void MoveX(int cord)
+    {
+        X = X + cord;
+    }
+    public void MoveY(int cord)
+    {
+        Y = Y + cord;
+    }
+    public string GetLocation()
+    {
+        return string.Format("location de cette point est {0},{1}", X, Y);
+    }
+}
+
+    
+
+}
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
     }
 }
